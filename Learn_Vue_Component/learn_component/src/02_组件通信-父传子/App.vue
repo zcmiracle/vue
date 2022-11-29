7b4e8941fde46e7cf490f36428814162dfed4de4
Fearless <489368600@qq.com>
2022-11-29 10:37:23 +0800

    组件通信-父传子

---
+++ Learn_Vue_Component/learn_component/src/02_组件通信-父传子/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <show-info name="James" :age="38" :height="203" address="克利夫兰" class="active">
+  </show-info>
+
+  <show-info name="MJ" :age="50" :height="198">
+  </show-info>
+
+  <show-info :age="100" show-message="哈哈哈哈">
+  </show-info>
+
+</template>
+
+
+<script>
+  import ShowInfo from './ShowInfo.vue'
+  export default {
+    components: {
+      ShowInfo
+    }
+  }
+</script>
+
+
+<style scoped>
+</style>
---
+++ Learn_Vue_Component/learn_component/src/02_组件通信-父传子/ShowInfo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="info">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <h2>身高：{{ height }}</h2>
+
+    <h2>Message: {{ showMessage }}</h2>
+  </div>
+
+  <div class="others" v-bind="$attrs"></div>
+</template>
+
+
+<script>
+  export default {
+    // inheritAttr: false,
+
+    // 作用：接收父组件传递过来的属性
+    // 1、props 数组语法
+    // 弊端：1.不能对类型进行验证 2.没有默认值
+    // props: ["name", "age", "height"]
+
+    props: {
+      name: {
+        type: String,
+        default: "我的默认的name-张三"
+      },
+      age: {
+        type: Number,
+        default: 0
+      },
+      height: {
+        type: Number,
+        default: 3
+      },
+      
+      // 重要原则：
+      // 对象类型写默认值时候，需要编写defalut的函数，函数返回默认值
+      friend: {
+        type: Object,
+        default() {
+          return { 
+            name : "James" 
+          }
+        }
+      },
+      hobbies: {
+        type: Array,
+        default: () => ["篮球", "足球", "羽毛球"]
+      },
+
+      showMessage: {
+        type: String,
+        default: "我是showMessage"
+      }
+
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
