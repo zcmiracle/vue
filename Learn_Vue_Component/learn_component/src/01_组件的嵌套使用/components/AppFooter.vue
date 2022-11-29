03ceaac953b056495dd96611825d7ad44c41a623
Fearless <489368600@qq.com>
2022-11-29 09:57:08 +0800

    组件使用

---
+++ Learn_Vue_Component/learn_component/src/01_组件的嵌套使用/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div>物流配送</div>
+    <div>版权声明</div>
+    <div>免责声明</div>
+    <div>注册地址：广东省深圳市福田区</div>
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+
+  }
+
+</script>
+
+<style scoped>
+
+  .footer {
+    background-color: green;
+  }
+
+</style>
--- Learn_Vue_Component/learn_component/src/main.js
+++ Learn_Vue_Component/learn_component/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+
+
+// import App from './App.vue'
+import App from './01_组件的嵌套使用/App.vue'
 
 createApp(App).mount('#app')
